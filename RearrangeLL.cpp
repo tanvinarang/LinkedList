8d06452a57c875b97bf57eab6c3262fca74563f6
Tanvi <narangtanvi19@gmail.com>
2015-10-05 02:52:18 +0530

    Adding revLL.cpp

--- RearrangeLL.cpp
+++ RearrangeLL.cpp
@@ -1,3 +1,17 @@
+Given a singly linked list L0 -> L1 -> … -> Ln-1 -> Ln. Rearrange the nodes in the list so that the new formed list is : L0 -> Ln -> L1 -> Ln-1 -> L2 -> Ln-2 …
+
+You are required do this in-place without altering the nodes’ values.
+
+Examples:
+Input:  1 -> 2 -> 3 -> 4
+Output: 1 -> 4 -> 2 -> 3
+
+Input:  1 -> 2 -> 3 -> 4 -> 5
+Output: 1 -> 5 -> 2 -> 4 -> 3 
+
+
+
+
 #include<iostream>
 using namespace std;
 
